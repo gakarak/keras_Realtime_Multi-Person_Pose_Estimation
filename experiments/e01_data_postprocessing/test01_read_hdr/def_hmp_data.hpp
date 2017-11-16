fb1014a41989f509f7dad5eccdb159528eacf081
Alexander Kalinovsky (deepw) <gakarak@gmail.com>
2017-11-16 20:44:49 +0300

    ex

---
+++ experiments/e01_data_postprocessing/test01_read_hdr/def_hmp_data.hpp
@@ -0,0 +1,18 @@
+#ifndef DEF_HMP_DATA
+#define DEF_HMP_DATA
+
+#include <vector>
+
+struct SimpleData{
+	unsigned int rows;
+	unsigned int cols;
+	unsigned int channels;
+	std::vector<double> data;
+};
+
+extern SimpleData HMP_CF;
+extern SimpleData HMP_CL;
+extern SimpleData PAF_CF;
+extern SimpleData PAF_CL;
+
+#endif // DEF_HMP_DATA
---
+++ experiments/e01_data_postprocessing/test01_read_hdr/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "def_hmp_data.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    SimpleData data;
+    return 0;
+}
